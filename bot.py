1d88d93510da21e4823868229f139db4eb1098c4
seyedreza6567 <zariva1365@gmail.com>
2026-08-06 00:53:28 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,9 +19,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     )
 
 
+from telegram.ext import MessageHandler, filters
+
+async def messages(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+
+    if text == ":chart_with_upwards_trend: سیگنال‌ها":
+        await update.message.reply_text(get_btc_price())
+
+    elif text == ":moneybag: قیمت‌ها":
+        await update.message.reply_text("بخش قیمت‌ها به‌زودی تکمیل می‌شود.")
 TOKEN = "8656837062:AAHEpTcYOsWkyW_ZXi3fIIcD6AtA_YnZU4Y"
 app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, messages))
 
 app.run_polling()
