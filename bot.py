9945357506e39b33fee661e116ed04b9f5710980
seyedreza6567 <zariva1365@gmail.com>
2026-08-05 18:04:47 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,24 @@
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import Application, CommandHandler, ContextTypes
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ربات فعال شد :white_check_mark:")
+    keyboard = [
+        [":chart_with_upwards_trend: سیگنال‌ها", ":moneybag: قیمت‌ها"],
+        ["⚙ تنظیمات"]
+    ]
 
-TOKEN = "8656837062:AAHEpTcYOsWkyW_ZXi3fIIcD6AtA_YnZU4Y"
+    reply_markup = ReplyKeyboardMarkup(
+        keyboard,
+        resize_keyboard=True
+    )
 
-app = Application.builder().token(TOKEN).build()
+    await update.message.reply_text(
+        "ربات سیگنال فیوچرز فعال شد :white_check_mark:\n\nیک گزینه را انتخاب کن:",
+        reply_markup=reply_markup
+    )
+
+
+TOKEN = "8656837062:AAHEpTcYOsWkyW_ZXi3fIIcD6AtA_YnZU4Y"app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 
