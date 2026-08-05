73ce846e1a227e46add154d176f7b2b08026c820
seyedreza6567 <zariva1365@gmail.com>
2026-08-06 00:20:26 +0330

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,9 @@
-# Scanner Toobit
+import requests
+
+def test_connection():
+    url = "https://api.toobit.com"
+    try:
+        response = requests.get(url, timeout=10)
+        return response.status_code
+    except Exception as e:
+        return str(e)
