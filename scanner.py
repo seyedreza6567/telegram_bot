5ac502ae4feb31f879989bb32c2b95de20bdd945
seyedreza6567 <zariva1365@gmail.com>
2026-08-06 01:01:14 +0330

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,13 +1,19 @@
 import requests
 
 def get_btc_price():
-    url = "https://api.toobit.com/api/v1/ticker?symbol=BTCUSDT"
+    url = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT"
 
     try:
         r = requests.get(url, timeout=10)
+
+        if r.status_code != 200:
+            return f"خطای سرور: {r.status_code}"
+
         data = r.json()
 
-        return f"قیمت BTC: {data}"
+        price = data["price"]
+
+        return f":moneybag: قیمت BTC/USDT:\n{price} دلار"
 
     except Exception as e:
         return f"خطا: {e}"
