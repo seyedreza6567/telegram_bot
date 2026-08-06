7402b9fd8e54d1cabe9615744009e9cbc796bc71
seyedreza6567 <zariva1365@gmail.com>
2026-08-06 11:04:19 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from config import BOT_TOKEN
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
     Application,
@@ -7,7 +8,6 @@ from telegram.ext import (
     filters,
 )
 
-from config import BOT_TOKEN
 from scanner import get_btc_price
 
 
@@ -84,10 +84,8 @@ app = Application.builder().token(BOT_TOKEN).build()
 
 
 app.add_handler(CommandHandler("start", start))
-
 app.add_handler(
     MessageHandler(filters.TEXT & ~filters.COMMAND, messages)
 )
-
 
 app.run_polling()
