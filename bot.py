6f4e07ff3d02041ffb39465c705fb5f149442b94
seyedreza6567 <zariva1365@gmail.com>
2026-08-10 16:30:02 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -248,12 +248,13 @@ async def messages(
         return
 
         if text == "💰 قیمت‌ها":
+
             try:
 
-                df = get_klines(
-                    symbol=SYMBOL,
-                    interval="1h",
-                    limit=5
+            df = get_klines(
+                symbol=SYMBOL,
+                interval="1h",
+                limit=5
             )
 
             if df is not None:
